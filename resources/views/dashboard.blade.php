611629eb64c46c6701945ed0c09a87b93f1f0684
Rafael Costa <rcostadesignweb@gmail.com>
2022-11-08 10:03:07 -0300

    :sparkles: feat: Configurado mapbox

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,10 +3,15 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-3 col-sm-3 col-3">
+    <!-- TODO: Ver uma forma de carregar esses arquivos somente nas mágina -->
+    <script src='https://api.mapbox.com/mapbox-gl-js/v2.6.0/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v2.6.0/mapbox-gl.css' rel='stylesheet' />
 
-            <div class="card card-primary mb-0">
+
+    <div class="row">
+        <div class="col-md-3 col-sm-3 col-3 pt-2">
+
+            <div class="card card-primary">
                 <div class="card-header">
                     <h3 class="card-title">
                         Atividades sem endereço
@@ -18,7 +23,7 @@
                 </div>
             </div>
 
-            <div class="card card-primary mb-0">
+            <div class="card card-primary">
                 <div class="card-header">
                     <h3 class="card-title">
                         Metropolitana 1
@@ -30,7 +35,7 @@
                 </div>
             </div>
 
-            <div class="card card-primary mb-0">
+            <div class="card card-primary">
                 <div class="card-header">
                     <h3 class="card-title">
                         Mucuripe
@@ -42,7 +47,7 @@
                 </div>
             </div>
 
-            <div class="card card-primary mb-0">
+            <div class="card card-primary">
                 <div class="card-header">
                     <h3 class="card-title">
                         Centro
@@ -56,14 +61,21 @@
 
         </div>
         <div class="col-md-6 col-sm-6 col-6">
-            Aqui é o mapa
+            <x-mapbox id="mapId" style="left: 0; width: 100%; height: 800px!important;" :center="['long' => -38.5891587, 'lat' => -3.7900979]" :zoom="11" />
         </div>
-        <div class="col-md-3 col-sm-3 col-3">
-            Roteiros
+        <div class="col-md-3 col-sm-3 col-3 pt-2">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">
+                        Allan Formiga
+                    </h3>
+                </div>
+
+                <div class="card-body">
+                    Atividades aqui
+                </div>
+            </div>
         </div>
     </div>
 @stop
 
-@section('css')
-
-@stop
