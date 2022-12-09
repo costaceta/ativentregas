4f8d385edd356b836e00c1840a52e240796d1c83
André Vilaço <kalu80837@gmail.com>
2022-12-08 22:42:27 -0300

    Criado configurações dos tipos de atividades

--- resources/js/Pages/ActivityType/Create.jsx
+++ resources/js/Pages/ActivityType/Create.jsx
@@ -106,23 +106,131 @@ export default function Index(props) {
                             <CAccordionItem itemKey={2}>
                                 <CAccordionHeader>CAMPOS DA ATIVIDADE</CAccordionHeader>
                                 <CAccordionBody>
-                                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the
-                                    collapse plugin adds the appropriate classes that we use to style each element. These classes
-                                    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                                    modify any of this with custom CSS or overriding our default variables. It's also worth noting
-                                    that just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                                    does limit overflow.
+                                    <CRow>
+                                        <CCol className="mb-3" md={12}>
+                                            
+                                            <CFormSelect label="Código da atividade ?" aria-label="Default select example">
+                                                <option value="1">Obrigatório, inicia preenchido com um código padrão</option>
+                                                <option value="2">Obrigatório, inicia vazio</option>
+                                            </CFormSelect>
+                                        </CCol>
+
+                                        <CCol className="mb-3" md={12}>
+                                            <CFormSelect label="Tem campos para identificar o local da atividade?" aria-label="Default select example">
+                                                <option value="0">Não</option>
+                                                <option value="1">Código, Fantasia, Razão Social, Telefone</option>
+                                                <option value="2">Código, Razão Social, Telefone</option>
+                                            </CFormSelect>
+
+                                        </CCol>
+
+                                        <CCol className="mb-3" md={12}>
+                                            <CFormSelect label="Tem campos para endereço do destino?" aria-label="Default select example">
+                                                    <option value="0">Não</option>
+                                                    <option value="1">Logradouro, número, complemento, bairro, cidade, UF</option>
+                                                </CFormSelect>
+                                        </CCol>
+
+                                        <CCol className="mb-3" md={12}>
+                                            <CFormSelect label="Tem CEP de destino?" aria-label="Default select example">
+                                                <option value="0">Não</option>
+                                                <option value="1">Sim, depois dos campos de endereço</option>
+                                                <option value="2">Sim, antes dos campos de endereço</option>
+                                            </CFormSelect>
+                                        </CCol>
+
+                                        <CCol className="mb-3" md={12}>
+                                            <CFormSelect label="Tem data hora de faturamento?" aria-label="Default select example">
+                                                    <option value="0">Não</option>
+                                                    <option value="1">Sim</option>
+                                                </CFormSelect>
+                                        </CCol>
+
+                                        <CCol className="mb-3" md={12}>
+                                            <CFormSelect label="Tem campos para endereço de origem ?" aria-label="Default select example">
+                                                    <option value="0">Não</option>
+                                                    <option value="1">Sim</option>
+                                                </CFormSelect>
+                                        </CCol>
+
+                                        <CCol className="mb-3" md={12}>
+                                            <CFormSelect label="Tem peso?" aria-label="Default select example">
+                                                    <option value="0">Não</option>
+                                                    <option value="1">Sim</option>
+                                                </CFormSelect>
+                                        </CCol>
+
+                                        <CCol className="mb-3" md={12}>
+                                            <CFormSelect label="Tem volume?" aria-label="Default select example">
+                                                    <option value="0">Não</option>
+                                                    <option value="1">Sim</option>
+                                                </CFormSelect>
+                                        </CCol>
+
+                                        <CCol className="mb-3" md={12}>
+                                            <CFormSelect label="Tem código do orçamento?" aria-label="Default select example">
+                                                    <option value="0">Não</option>
+                                                    <option value="1">Sim</option>
+                                                </CFormSelect>
+                                        </CCol>
+
+                                        <CCol className="mb-3" md={12}>
+                                            <CFormSelect label="Tem valor da nota?" aria-label="Default select example">
+                                                    <option value="0">Não</option>
+                                                    <option value="1">Sim</option>
+                                                </CFormSelect>
+                                        </CCol>
+
+                                        
+                                    </CRow>
                                 </CAccordionBody>
                             </CAccordionItem>
                             <CAccordionItem itemKey={3}>
                                 <CAccordionHeader>DISPLAYS</CAccordionHeader>
                                 <CAccordionBody>
-                                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the
-                                    collapse plugin adds the appropriate classes that we use to style each element. These classes
-                                    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                                    modify any of this with custom CSS or overriding our default variables. It's also worth noting
-                                    that just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                                    does limit overflow.
+                                    <CRow>
+                                        <CCol className="mb-3" md={12}>
+                                            <CFormSelect label="Display 1 - no APP" aria-label="Default select example">
+                                                        <option value="0">0 - NÃO EXIBIR INFORMAÇÃO</option>
+                                                        <option value="1">1 - logradouro, numero, bairro</option>
+                                                        <option value="2">2 - logradouro, numero, bairro, cidade/municipio</option>
+                                                        <option value="3">3 - logradouro, numero, bairro, (hora)</option>
+                                                        <option value="4">4 - logradouro, numero, bairro, cidade/municipio, (hora)</option>
+                                                        <option value="5">5 - logradouro, numero, bairro, (data hora)</option>
+                                                        <option value="6">6 - logradouro, numero, bairro, cidade/municipio, (data hora)</option>
+                                                        <option value="7">7 - logradouro, numero, bairro, (data hora para realização da atividade)</option>
+                                                        <option value="8">8 - logradouro, numero, bairro, cidade/municipio, (data hora para realização da atividade)</option>
+                                                        <option value="9">9 - logradouro, numero, complemento, bairro, (data hora para realização da atividade)</option>
+                                                        <option value="10">10 - Nome do tomador do serviço</option>
+                                                        <option value="11">11 - Nome do tomador do serviço, data hora da solicitação</option>
+                                                        <option value="12">12 - Nome do tomador do serviço, hora da solicitação</option>
+                                                        <option value="13">13 - Nome do tomador do serviço, data hora da solicitação e observações</option>
+                                                        <option value="14">14 - Nome do tomador do serviço, hora da solicitação e observações</option>
+                                                        <option value="15">15 - Observações</option>
+                                                        <option value="16">16 - Tipo atividade (código da atividade), observação</option>
+                                                        <option value="33">33 - Nome do tomador do serviço, data hora da solicitação e observações (quebra de linha antes da obs)</option>
+                                                        <option value="34">34 - Nome do tomador do serviço, hora da solicitação e observações (quebra de linha antes da obs)</option>
+                                                        <option value="35">35 - Nome do tomador do serviço (código da atividade)</option>
+                                                        <option value="36">36 - Nome do tomador do serviço (código da atividade), hora da solicitação</option>
+                                                        <option value="37">37 - Nome do tomador do serviço (código da atividade), telefone, hora da solicitação</option>
+                                                        <option value="38">38 - Tipo atividade, Nome do tomador do serviço (código da atividade), telefone, hora da solicitação</option>
+                                                        <option value="40">40 - logradouro, numero, complemento, bairro, observação</option>
+                                                        <option value="41">41 - logradouro, numero, complemento, bairro, cidade, data hora solicitação, observação</option>
+                                                        <option value="42">42 - logradouro, numero, complemento, bairro, cidade, data hora solicitação, observação, Vols.: #</option>
+                                                        <option value="45">45 - Nome do usuário do sistema que cadastrou a atividade - observação</option>
+                                                        <option value="46">46 - Nome do usuário do sistema que cadastrou a atividade - Nome do tomador do serviço</option>
+                                                        <option value="47">47 - Nome do usuário do sistema que cadastrou a atividade - Nome do tomador do serviço - observação</option>
+                                                        <option value="48">48 - Nome do usuário do sistema que cadastrou a atividade - Nome do tomador do serviço (código da atividade) - observação</option>
+                                                        <option value="50">50 - Nome do usuário do sistema que cadastrou a atividade</option>
+                                                        <option value="60">60 - Razão Social do cliente</option>
+                                                        <option value="70">70 - Código da atividade, data hora da solicitação, origem</option>
+                                                        <option value="80">80 - Nome fantasia da loja/endereço de origem (código da atividade) - observação</option>
+                                                        <option value="81">81 - Nome fantasia da loja/endereço de origem (código da atividade) - Nome do tomador do serviço</option>
+                                                    </CFormSelect>
+
+                                        </CCol>
+                                        
+                                    </CRow>
                                 </CAccordionBody>
                             </CAccordionItem>
                             <CAccordionItem itemKey={4}>
