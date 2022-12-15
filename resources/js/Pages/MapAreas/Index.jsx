09819448eaa8232083ca2045ca4120cd79c5f2fb
Rafael Costa <rcostadesignweb@gmail.com>
2022-12-15 09:46:14 -0300

    feat: Criado listagem de usuários móveis

--- resources/js/Pages/MapAreas/Index.jsx
+++ resources/js/Pages/MapAreas/Index.jsx
@@ -19,6 +19,9 @@ import {
     CCardTitle,
     CRow,
     CCol,
+    CForm,
+    CFormInput,
+    CFormSelect,
 } from "@coreui/react";
 
 // TODO: Add token to environment variables
@@ -31,47 +34,83 @@ export default function Index(props) {
     const [lng, setLng] = useState(-38.5187);
     const [lat, setLat] = useState(-3.7262);
     const [zoom, setZoom] = useState(12);
-    const [mapAreas, setMapAreas] = useState(['']);
+    const [mapAreas, setMapAreas] = useState([]);
 
-    const [local, setLocal] = useState([
-        [
-            [
-                -38.59939558166275,
-                -3.731417028211851
-            ],
-            [
-                -38.56985958511166,
-                -3.701943152416078
-            ],
-            [
-                -38.5160889247247,
-                -3.712145759295609
-            ],
-            [
-                -38.551304920612125,
-                -3.75484427579822
-            ],
-            [
-                -38.569480918489035,
-                -3.7631570191955177
-            ],
-            [
-                -38.59939558166275,
-                -3.731417028211851
-            ]
-        ]
-    ]);
+    const [local, setLocals] = useState();
+    const [featuresMap, setFeaturesMap] = useState();
 
+    const areas = {
+        'type': 'FeatureCollection',
+        'features': [
+            {
+                'type': "Feature",
+                'geometry': {
+                    'type': "Polygon",
+                    'coordinates': [
+                        [
+                            [
+                                -38.522094521525474,
+                                -3.7570221591734736
+                            ],
+                            [
+                                -38.51687997640599,
+                                -3.71109583702669
+                            ],
+                            [
+                                -38.62049072072628,
+                                -3.685982402880853
+                            ],
+                            [
+                                -38.62389151102167,
+                                -3.758153318991873
+                            ],
+                            [
+                                -38.522094521525474,
+                                -3.7570221591734736
+                            ]
+                        ]
+                    ]
+                },
+            },
+            {
+                'type': "Feature",
+                'geometry': {
+                    'type': "Polygon",
+                    'coordinates': [
+                        [
+                            [
+                                -38.621041606791294,
+                                -3.685177681824854
+                            ],
+                            [
+                                -38.712414789004015,
+                                -3.6378474613396037
+                            ],
+                            [
+                                -38.72720854231454,
+                                -3.7585562443315723
+                            ],
+                            [
+                                -38.62495760031476,
+                                -3.757687894834504
+                            ],
+                            [
+                                -38.621041606791294,
+                                -3.685177681824854
+                            ]
+                        ]
+                    ]
+                },
+            },
+        ],
+    }
 
     const draw = new MapboxDraw({
         displayControlsDefault: false,
-        // Select which mapbox-gl-draw control buttons to add to the map.
         controls: {
             polygon: true,
             trash: true
         },
-        // Set mapbox-gl-draw to draw by default.
-        // The user does not have to click the polygon control button first.
         defaultMode: 'draw_polygon'
     });
 
@@ -81,43 +120,23 @@ export default function Index(props) {
             container: mapContainer.current,
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [lng, lat],
-            zoom: zoom
+            zoom: zoom,
         });
 
         map.addControl(draw)
 
-        map.on('draw.create', updateArea);
-
         map.on("load", () => {
-            // Add a data source containing GeoJSON data.
-            map.addSource("maine", {
+            map.addSource("map-areas", {
                 type: "geojson",
-                data: {
-                    type: "Feature",
-                    geometry: {
-                        type: "Polygon",
-                        // These coordinates outline Maine.
-                        coordinates: local,
-                    },
-                },
+                data: areas,
             });
 
-            // Add a new layer to visualize the polygon.
+            draw.add(areas)
+
             map.addLayer({
-                id: "maine",
-                type: "fill",
-                source: "maine", // reference the data source
-                layout: {},
-                paint: {
-                    "fill-color": "#0080ff", // blue color fill
-                    "fill-opacity": 0.5,
-                },
-            });
-            // Add a black outline around the polygon.
-            map.addLayer({
-                id: "outline",
+                id: "map-areas-outline",
                 type: "line",
-                source: "maine",
+                source: "map-areas",
                 layout: {},
                 paint: {
                     "line-color": "#000",
@@ -126,105 +145,26 @@ export default function Index(props) {
             });
         });
 
-        map.on('click', () => {
-            // const layerMeta = map.getSource('maine')._data.geometry.coordinates;
-            // console.log(layerMeta)
 
-            console.log(map.getStyle().sources)
+        map.on('draw.create', () => {
+            let data = draw.getAll();
+            map.getSource('map-areas').setData(data);
+            setMapAreas([...mapAreas, {title: 'Rafael'}])
+        });
 
-            // map.removeLayer('maine')
-            // map.removeLayer('outline')
-            // map.removeSource('maine')
-        })
+        map.on('draw.update', () => {
+            let data = draw.getAll();
+            map.getSource('map-areas').setData(data);
+        });
+
+        map.on('draw.delete', () => {
+            let data = draw.getAll();
+            map.getSource('map-areas');
+        });
 
         return () => map.remove();
 
     }, []);
-
-    // useEffect(() => {
-    //     if (!map.current) return; // wait for map to initialize
-    //     map.current.on("move", () => {
-    //         console.log('lng', map.current);
-    //         setLng(map.current.getCenter().lng.toFixed(4));
-    //         setLat(map.current.getCenter().lat.toFixed(4));
-    //         setZoom(map.current.getZoom().toFixed(2));
-    //     });
-    // });
-
-    const updateArea = ({ features }) => {
-        console.log("features", features)
-
-        let data = draw.getAll();
-        console.log("All", data);
-    };
-
-    const clickMap = (data) => {
-        console.log("Click Data", data)
-        console.log("map", map)
-    }
-
-    const onDrawCreate = ({ features }) => {
-        let data = draw.getAll();
-        const polygonData = data.features[0].geometry.coordinates;
-        drawPolygon(polygonData);
-    };
-
-    const onDrawUpdate = ({ features }) => {
-        let data = draw.getAll();
-        map.current.removeLayer('maine').removeSource('maine');
-        const polygonData = data.features[0].geometry.coordinates;
-        drawPolygon(polygonData);
-    };
-
-    const onDrawDelete = ({ features }) => {
-        let data = draw.getAll();
-        map.current.removeLayer('maine').removeSource('maine');
-    };
-
-    const drawPolygon = (points) => {
-        map.current.addLayer({
-            'id': 'maine',
-            'type': 'fill',
-            'source': {
-                'type': 'geojson',
-                'data': {
-                    'type': 'Feature',
-                    'geometry': {
-                        'type': 'Polygon',
-                        'coordinates': points
-                    }
-                }
-            },
-            'layout': {},
-            'paint': {
-                'fill-color': '#088',
-                'fill-opacity': 0.3,
-            }
-        });
-
-        map.current.addLayer({
-            id: "maine",
-            type: "line",
-            source: "maine",
-            layout: {},
-            paint: {
-                "line-color": "#000",
-                "line-width": 3,
-            },
-        });
-
-        map.current.addLayer({
-            id: "label",
-            type: "symbol",
-            source: "maine",
-            layout: {
-                "text-field": "Label",
-            },
-            paint: {
-                "text-color": "black",
-            },
-        });
-    }
 
     return (
         <>
@@ -239,17 +179,42 @@ export default function Index(props) {
                                     <CCard key={index} className="mb-3">
                                         <CCardHeader>
                                             <strong>
-                                                Listagem de áreas do mapa
+                                                #1
                                             </strong>
                                         </CCardHeader>
                                         <CCardBody>
-                                            Lorem ipsum, dolor sit amet
-                                            consectetur adipisicing elit.
-                                            Voluptate ullam quidem, et, voluptas
-                                            enim ea cupiditate fuga consequatur
-                                            esse quod modi id porro doloremque
-                                            qui debitis? Illo natus molestiae
-                                            vel.
+                                            <CForm>
+                                                <CFormInput
+                                                    className="mb-3"
+                                                    type="text"
+                                                    label="Titulo/Descrição"
+                                                />
+                                                <CFormInput
+                                                    className="mb-3"
+                                                    type="text"
+                                                    label="Pequeno rótulo"
+                                                />
+                                                <CFormSelect
+                                                    className="mb-3"
+                                                    label="Lógica:"
+                                                >
+                                                    <option value="1">Área próxima 1</option>
+                                                    <option value="2">Área próxima 2</option>
+                                                    <option value="3">Área próxima 3</option>
+                                                    <option value="3">Área próxima 4</option>
+                                                    <option value="3">Área próxima 5 - Uma coleta valida o esquema de área</option>
+                                                </CFormSelect>
+                                                <CFormInput
+                                                    className="mb-3"
+                                                    type="text"
+                                                    label="Taxa para o Solicitante (R$)"
+                                                />
+                                                <CFormInput
+                                                    className="mb-3"
+                                                    type="text"
+                                                    label="Taxa para o Agente (R$)"
+                                                />
+                                            </CForm>
                                         </CCardBody>
                                     </CCard>
                                 ))}
